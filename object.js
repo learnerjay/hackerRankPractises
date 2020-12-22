fc01cf7a26cd4e9ed7b5ca03ec19fd4b98c40090
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2020-12-22 22:54:49 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,11 +1,11 @@
-\**
+/**
 Task
 Complete the function in the editor. It has two parameters: and . It must return an object modeling a rectangle that has the following properties:
 length : This value is equal to a.
 width : This value is equal to b.
 perimeter : This value is equal to 2 X (a + b)
 area : This value is equal to a X b
-Note: The names of the object’s properties must be spelled correctly to pass this challenge.
+Note: The names of the objectâ€™s properties must be spelled correctly to pass this challenge.
 **/
 Solution
 
