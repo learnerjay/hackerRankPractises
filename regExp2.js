87cfbbbc42e4bab4e1519fc0e94e67f27d0575fd
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-01-01 22:39:35 +0530

    Add files via upload
    - Regexp Practice
    (Global pattern & Digit Matching)

---
+++ regExp2.js
@@ -0,0 +1,55 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    var re = /\d+/g;
+    /**
+     * /
+\d+matches a digit (equal to [0-9])
++ Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+Global pattern flags
+g modifier: global. All matches (don't return after first match)
+     **/
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    const r = s.match(re);
+    
+    for (const e of r) {
+        console.log(e);
+    }
+}
