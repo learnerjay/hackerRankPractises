ed719c89865ff77e148178c5eb88ef6072bdf5c9
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-01-02 22:55:18 +0530

    Update regExp3.js
    Added * for covering 1 more test case 
    - providing more than 1 favourable conditions.
    - Matches end of input.

--- regExp3.js
+++ regExp3.js
@@ -28,7 +28,7 @@ function regexVar() {
      * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
      * followed by one or more letters.
      */
-   var re = /^(Mr|Mrs|Ms|Dr|Er)(\.)[A-Za-z]/
+   var re = /^(Mr|Mrs|Ms|Dr|Er)(\.)[A-Za-z]*$/
     
     /*
      * Do not remove the return statement
