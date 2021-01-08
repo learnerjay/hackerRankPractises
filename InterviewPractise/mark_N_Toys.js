188b5422bea12acc70cf4f3252f7aed835143d73
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-01-08 22:37:21 +0530

    Add files via upload
    Solved the problem using counting the sum should be lesser than the Capital amount

---
+++ InterviewPractise/mark_N_Toys.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the maximumToys function below.
+function maximumToys(prices, k) {
+    
+  let count=0;
+  let sum = 0 ;  
+ //Arrays.sort(prices);  
+ for(let i=0;i<prices.length;i++)
+ {
+     if(sum + prices[i] <= k)
+     {
+         sum=sum+prices[i];
+         count++;
+     }
+
+
+ }
+ return count;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const prices = readLine().split(' ').map(pricesTemp => parseInt(pricesTemp, 10));
+
+    let result = maximumToys(prices, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
