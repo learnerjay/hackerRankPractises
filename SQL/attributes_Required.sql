4689f334ea6991a99facca920854527c3cecfaf5
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-01-25 21:20:08 +0530

    Add files via upload
    Attributes as required from table

---
+++ SQL/attributes_Required.sql
@@ -0,0 +1,19 @@
+/*
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+The CITY table is described as follows:
+
+*/
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
+/*
+Your Output (stdout)
+
+    Neyagawa
+
+    Ageo
+
+    Sayama
+
+    Omuta
+
+    Tokuyama
+*/
