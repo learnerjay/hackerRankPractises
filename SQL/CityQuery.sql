ee2b2baad25ccfddd84797891f062c188f1d8045
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-01-28 22:07:48 +0530

    Add files via upload
    Basic Sql query

---
+++ SQL/CityQuery.sql
@@ -0,0 +1,11 @@
+/*
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+Solution
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT CITY,STATE FROM STATION ;  
