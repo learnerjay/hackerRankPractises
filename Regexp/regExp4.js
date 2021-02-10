d3e3ba46bfa0d69cf6810dc624a72fbfa1eb9a8e
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-02-10 22:25:30 +0530

    Add files via upload
    Solved charcaters & nums with [.] at character limit

---
+++ Regexp/regExp4.js
@@ -0,0 +1,33 @@
+Matching Start & End
+Points: 20Rank: 50418
+Problem
+Submissions
+Leaderboard
+Discussions
+Editorial
+
+The ^ symbol matches the position at the start of a string.
+
+[ach06_01.png]
+
+The $ symbol matches the position at the end of a string.
+
+[ach06_02.png]
+
+Task
+
+You have a test string
+. Your task is to match the pattern
+Here, denotes a word character, and denotes a digit.
+must start with a digit and end with . symbol.
+should be
+
+characters long only.
+
+Note
+
+This is a regex only challenge. You are not required to write code.
+You have to fill the regex pattern in the blank (_________).
+
+Regexp
+var Regex_Pattern = /^\d{1}\w{4}[.]{1}$/;
