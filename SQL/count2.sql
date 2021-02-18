53b6ae35327a25b9d14f7b59a6abcf77e7f1d770
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-02-18 22:45:22 +0530

    Add files via upload
    Count for the table

---
+++ SQL/count2.sql
@@ -0,0 +1,7 @@
+Query a count of the number of cities in CITY having a Population larger than 100000.
+
+Input Format
+
+The CITY table is described as follows: 
+Solution
+SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000;
