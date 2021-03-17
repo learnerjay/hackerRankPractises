1508dda18b1a83f302fdf34834ea324099d3def5
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-03-17 21:30:48 +0530

    Add files via upload
    RLIKE exp for checking the characters required for case

---
+++ SQL/WeatherObservationStation7.sql
@@ -0,0 +1,17 @@
+Weather Observation Station 7
+
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+[Station.jpg]
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+Solution
+/*
+Enter your query here.
+*/
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY RLIKE '[AEIOU]$';
