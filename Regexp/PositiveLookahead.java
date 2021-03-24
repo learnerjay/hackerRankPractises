8ff563d71e339953a3c7601913c9d7a6851607f8
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-03-24 19:36:37 +0530

    Add files via upload
    We need to find only those matches for a pattern that are followed or preceded by another pattern.
    
    There’s a special syntax for that, called “lookahead” 
    --- regexp1(?=regexp2)

---
+++ Regexp/PositiveLookahead.java
@@ -0,0 +1,50 @@
+/*
+Positive Lookahead
+
+The positive lookahead (?=) asserts regex_1 to be immediately followed by regex_2. The lookahead is excluded from the match. It does not return matches of regex_2. The lookahead only asserts whether a match is possible or not.
+
+[ach22.png]
+
+Task
+
+You have a test string S.
+Write a regex that can match all occurrences of o followed immediately by oo in S.
+
+Note
+
+This is a regex only challenge. You are not required to write code.
+You have to fill the regex pattern in the blank (_________).
+*/
+Solution
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("o(?=oo)"); //Use '\\' instead of '\'.
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        int Count = 0;
+        while(m.find()){
+            Count += 1;
+        }
+        System.out.format("Number of matches : %d",Count);
+    }   
+    
+}
