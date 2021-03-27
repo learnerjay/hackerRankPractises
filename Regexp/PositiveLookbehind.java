f6dcd3bb94218e1434a44a49f854abf5bb224245
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-03-27 22:28:05 +0530

    Add files via upload
    - Positive lookbehind: (?<=Y)X, matches X, but only if there’s Y before it.
    The positive lookbehind (?<=) asserts regex_1 to be immediately preceded by regex_2. Lookbehind is excluded from the match (do not consume matches of regex_2), but only assert whether a match is possible or not.

---
+++ Regexp/PositiveLookbehind.java
@@ -0,0 +1,52 @@
+\*
+Positive Lookbehind
+
+The positive lookbehind (?<=) asserts regex_1 to be immediately preceded by regex_2. Lookbehind is excluded from the match (do not consume matches of regex_2), but only assert whether a match is possible or not.
+
+[ach24.png]
+
+Task
+
+You have a test String S.
+Write a regex which can match all the occurences of digit which are immediately preceded by odd digit.
+
+Note
+
+This is a regex only challenge. You are not required to write a code.
+You have to fill the regex pattern in the blank (_________).
+
+JavaScript do not support lookbehind.
+*/
+Solution
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(?<=[13579][1-9])"); //Use '\\' instead of '\'.
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        int Count = 0;
+        while(m.find()){
+            Count += 1;
+        }
+        System.out.format("Number of matches : %d",Count);
+    }   
+    
+}
