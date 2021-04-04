c5ae6520b62a82470fd00e1e31d1975c2367a9ba
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-04-04 21:38:48 +0530

    Add files via upload
    - Rounding up the average
    - average of an table

---
+++ SQL/AveragePopulation.sql
@@ -0,0 +1,8 @@
+Average Population
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+The CITY table is described as follows: 
+Solution
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
