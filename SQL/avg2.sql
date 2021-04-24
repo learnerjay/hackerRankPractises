03e64fb675d1286d1dbf5679371b4a3f2fbd200d
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-04-24 22:08:32 +0530

    Add files via upload
    Average the field from sql

---
+++ SQL/avg2.sql
@@ -0,0 +1,12 @@
+Revising Aggregations - Averages
+Query the average population of all cities in CITY where District is California.
+
+Input Format
+The CITY table is described as follows: 
+
+Solution
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT="CALIFORNIA";
